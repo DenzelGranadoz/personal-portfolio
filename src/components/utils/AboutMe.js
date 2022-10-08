9b7f4a7ab46ae16103d5fe2bd2bcada4420f8851
Denzel Granadoz <zelgranadoz@gmail.com>
2022-10-08 11:06:51 +0800

    destructured about me description and image

---
+++ src/components/utils/AboutMe.js
@@ -0,0 +1,9 @@
+import picture from '../../assets/images/about.jpeg';
+
+const about = {
+  description:
+    "<span>Hi!</span> My name is <span>Denzel</span> and my interest in web development only grew in the recent years courtesy of the pandemic. I started out tinkering with HTML and CSS and have enjoyed working on javascript projects in my free time. I'm now a self-taught <span> Front-end developer </span> with a<span> Computer Science Degree </span> from De La Salle University - Dasmarinas, Philippines.",
+  image: picture,
+};
+
+export default about;
