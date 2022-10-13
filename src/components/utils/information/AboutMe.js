cea570947929d3b97d71359bcfca9802d8ce2cc3
Denzel Granadoz <zelgranadoz@gmail.com>
2022-10-13 16:01:32 +0800

    updated about me information

--- src/components/utils/information/AboutMe.js
+++ src/components/utils/information/AboutMe.js
@@ -2,7 +2,7 @@ import picture from '../../../assets/images/about.jpeg';
 
 const about = {
   description:
-    "<span>Hi!</span> My name is <span>Denzel</span> and I enjoy playing video games and watching cat videos on the internet in my spare time. My web development journey started out with me tinkering with HTML and CSS and have enjoyed working on JavaScript projects. . I'm now a self-taught <span> Front-end developer </span> with a<span> Computer Science Degree </span> from De La Salle University - Dasmarinas, Philippines.",
+    "<span>Hi!</span> My name is <span>Denzel</span> and I enjoy playing video games and watching cat videos on the internet in my spare time. My <span>passion</span> in <span>Software Engineering</span> pushed me to <span>explore</span> Front-End development and my internship experience taught me how to be a <span>team player</span> under a <span>scrum</span> master. I'm now a self-taught <span> Front-end developer </span> with a<span> Computer Science Degree </span> from De La Salle University - Dasmarinas, Philippines.",
   image: picture,
   title: 'About me',
 };
