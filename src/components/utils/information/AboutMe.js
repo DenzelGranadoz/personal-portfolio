16d474c6556d2fa0c473147366340e5fe37b41d1
Denzel Granadoz <zelgranadoz@gmail.com>
2024-01-15 12:23:22 +0800

    updated about me

--- src/components/utils/information/AboutMe.js
+++ src/components/utils/information/AboutMe.js
@@ -2,7 +2,7 @@ import picture from '../../../assets/images/about.jpeg';
 
 const about = {
   description:
-    "<span>Hi!</span> My name is <span>Denzel</span> and I enjoy playing video games and watching cat videos on the internet in my spare time. My <span>passion</span> in <span>Software Engineering</span> pushed me to <span>explore</span> Front-End development and my internship experience taught me how to be a <span>team player</span> under a <span>scrum</span> master. I'm now a self-taught <span> Front-end developer </span> with a<span> Computer Science Degree </span> from De La Salle University - Dasmarinas, Philippines.",
+    '<span>Hi!</span> My name is <span>Denzel</span>, and I have a year of software development experience mainly working with <span>React</span> and <Span>Java</Span>. I have <span>strong work ethic</span> and have <span>collaborated with cross-functional teams</span> to <span>deliver</span> complex projects on time. I like to spend my free time doing <span>freelance</span> work and I <span>enjoy</span> playing video games or watching cat videos on the internet.',
   image: picture,
   title: 'About me',
 };
